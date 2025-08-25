46350922b7e47a80bd2e03950df275923a1f40a0
Chen Wu <chenw@sambanovasystems.com>
2025-08-25 18:05:09 +0000

    Update .gitignore to include eval log files, upgrade vllm version in requirements.txt, and add ruler_server_llama.sh for model serving configuration; modify ruler_server.sh to adjust model paths and parameters.

--- eval/ruler_server_llama.sh
+++ eval/ruler_server_llama.sh
@@ -9,16 +9,13 @@ export SPEC_CONFIG_PATH=../configs/config_p5_full.yaml
 # export SPEC_CONFIG_PATH=../configs/config_p7_full_lah8.yaml
 # export SPEC_CONFIG_PATH=../configs/config_p9_full.yaml
 
-# export ENABLE_SP=/nvmedata/hf_checkpoints/Llama-3.2-1B-Instruct
-# MODEL_PATH=/nvmedata/hf_checkpoints/Meta-Llama-3.1-8B-Instruct
-export ENABLE_SP=/nvmedata/hf_checkpoints/Qwen3-4B-Instruct-2507
-MODEL_PATH=Qwen/Qwen3-32B
+export ENABLE_SP=/nvmedata/hf_checkpoints/Llama-3.2-1B-Instruct
+MODEL_PATH=/nvmedata/hf_checkpoints/Meta-Llama-3.1-8B-Instruct
 PORT=5000
 
-export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1     # only needed if vLLM complains about max_model_len
+# export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1     # only needed if vLLM complains about max_model_len
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python ruler_server.py \
-    --rope-scaling '{"rope_type":"yarn","factor":4.0,"original_max_position_embeddings":32768}' \
     --model $MODEL_PATH \
     --tensor-parallel-size 4 \
     --gpu-memory-utilization 0.95 \
